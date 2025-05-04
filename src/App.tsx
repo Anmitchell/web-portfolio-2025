fa334fe2919bcb931e20c20d6985f6058a9dbc48
Anmitchell <avarynmitchell@gmail.com>
2025-05-04 19:53:31 -0400

    Intitial setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='app'>
+      <div className='container'>
+        <h1>Hello World</h1>
+        <p>This is a test</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
